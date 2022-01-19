2d67bfbf68e34fcbb1873b086667938eefb7385c
Your Name <86778428+aznmax@users.noreply.github.com>
2022-01-19 15:06:56 +0100

    update tags

--- eventList.js
+++ eventList.js
@@ -1,4 +1,4 @@
-import {recipes } from "./JSON/recipes.js";
+
 
 const liste = document.querySelectorAll('.ingredient');
 console.log(liste);
@@ -12,7 +12,6 @@ console.log(contIng);
 
 const inpIng = document.querySelector('.target_ing');
 console.log(inpIng);
-
 
 liste.forEach(function(item){
     item.addEventListener('click',function(){
@@ -32,5 +31,35 @@ liste.forEach(function(item){
             tagIng.style.display = "none";
         }) 
     })
+});
+
+// Tags Appareils // 
+
+const liste2 = document.querySelectorAll('.appliance');
+const applianceTag = document.querySelector('.tagAppareil');
+const elementTagApr = document.querySelector('.tagAppareil');
+const contApr = document.querySelector(".container_appareils");
+const inpApr = document.querySelector('.target_apr');
+
+liste2.forEach(function(item2){
+    item2.addEventListener('click',function(){
+        console.log(item2.innerText);
+        const tagApr = document.createElement('div');
+        tagApr.classList.add("tagButtonAppareil");
+        tagApr.innerText = `${item2.innerText}`;
+        elementTagApr.append(tagApr)
+        const elementIcon2 = document.createElement('div');
+        elementIcon2.classList.add("crossIcon2");
+        elementIcon2.innerHTML = `<i class="far fa-times-circle"></i>`;
+        console.log(elementIcon2);
+        tagApr.append(elementIcon2)
+        contApr.style.display = "none";
+        inpApr.style.display = "block";  
+        tagApr.addEventListener("click",function(){
+            tagApr.style.display = "none";
+        }) 
+    
+    })
 })
+
 
