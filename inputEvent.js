bf7e0db96a2099e2a3c28ed2736d49571fb8a369
Your Name <86778428+aznmax@users.noreply.github.com>
2022-01-26 17:17:16 +0100

    keyUp update

--- inputEvent.js
+++ inputEvent.js
@@ -3,13 +3,15 @@ console.log(input);
 const listeIng = document.querySelectorAll('.ingredient');
 console.log(listeIng);
 
-input.addEventListener('keyPress',updateValue,false);
+input.addEventListener('keyup', function(e) {
+  let val = e.target.value;
+  console.log(val);
+  listeIng.forEach(function(ing) {
+    if (ing.innerText.toLowerCase().indexOf(val.toLowerCase()) > -1) {
+      ing.style.display = 'block';
+    } else {
+      ing.style.display = 'none';
+    }
+  });
 
-function updateValue() {
-    input = document.querySelector('.inp_ing')
-    const inputElement = input.value;
-    listeIng.textContent = inputElement.value;
-    console.log(updateValue);
-  }
-  
-
+});
